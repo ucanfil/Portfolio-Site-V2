4d7cffd7dc0a1677a214317ebd5388ec232b6a81
Burak Tilek <burak.tilek@gmail.com>
2018-09-11 09:28:21 +0100

    Heading height and font-size changed

---
+++ js/app.js
@@ -0,0 +1,21 @@
+let header = document.querySelector('.intro-header');
+
+/*
+
+header.innerHTML = '';
+let headerText = ['Not your average web-developer.'];
+let text = headerText[0].split('');
+
+function addLetter() {
+    console.log(text);
+    if (header.innerHTML.length < 31) {
+        header.innerHTML += text.splice(0, 1);
+    }
+}
+
+
+
+
+let timerId = setInterval(addLetter, 150);
+
+*/
