26e8ea4a0cdc82ebfe057fac09485680ce2d153b
Burak Tilek <burak.tilek@gmail.com>
2018-09-12 16:26:01 +0100

    js file changed to be compatible with IE10

--- js/app.js
+++ js/app.js
@@ -1,22 +1,25 @@
+
 //======= H1 Style Solution for IE10 =======//
 var doc = document.documentElement;
 doc.setAttribute('data-useragent', navigator.userAgent);
 
 //======= Smooth Scroolling =======//
 // Src="https://stackoverflow.com/a/7717572"
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
+if (navigator.appVersion.indexOf('MSIE 10') === -1) {
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
         });
     });
-});
+}
 
 
-let header = document.querySelector('.intro-header');
 
 /*
+let header = document.querySelector('.intro-header');
 
 header.innerHTML = '';
 let headerText = ['Not your average web-developer.'];
