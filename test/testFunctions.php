6b8f20ff7032be0519331b56209517ae8edd6cd0
Burak Tilek <burak.tilek@gmail.com>
2018-10-04 13:15:34 +0100

    Test suites are redesigned

--- test/testFunctions.php
+++ test/testFunctions.php
@@ -4,20 +4,31 @@ require('../cms/nonDbFunctions.php');
 
 use PHPUnit\Framework\TestCase;
 
-class Cms extends TestCase {
+class Cms extends TestCase
+{
     public $projects = [
-        ['id'=> 4, 'title' => 'My Reads']
+        ['id'=> 4,
+        'p_title' => 'My Reads',
+        'bg_image_url' => '#',
+        'p_content' => 'Content',
+        'code_url' => '#',
+        'see_url' => '#',
+        ]
     ];
     // Success Tests
     public function testOKpopulateDropdown() {
         $html = populateDropdown($this->projects);
-        $this->assertContains('</option>', $html);
+        $this->assertEquals('<option value="4">My Reads</option>', $html);
     }
 
     public function testOKpopulateProject() {
         $html = populateProject($this->projects);
-        $this->assertContains('</li>', $html);
-        $this->assertContains('</h3>', $html);
+        $this->assertContains('<li class="project project4"', $html);
+        $this->assertContains('style="background-image: url(\'#\')">', $html);
+        $this->assertContains('<h3>My Reads</h3>', $html);
+        $this->assertContains('<p>Content</p>', $html);
+        $this->assertContains('<a href="#" target="_blank">CODE</a>', $html);
+        $this->assertContains('<a href="#" target="_blank">SEE ?</a>', $html);
     }
 
     public function testOKisKeyExist() {
@@ -30,14 +41,18 @@ class Cms extends TestCase {
     public function testFailurepopulateDropdown() {
         $projects = [[1, 4]];
         $html = populateDropdown($projects);
-        $this->assertContains('</option>', $html);
+        $this->assertEquals('<option value=""></option>', $html);
     }
 
     public function testFailurepopulateProject() {
         $projects = [[1, 4]];
         $html = populateProject($projects);
-        $this->assertContains('</li>', $html);
-        $this->assertContains('</h3>', $html);
+        $this->assertContains('<li class="project project"', $html);
+        $this->assertContains('style="background-image: url(\'\')">', $html);
+        $this->assertContains('<h3></h3>', $html);
+        $this->assertContains('<p></p>', $html);
+        $this->assertContains('<a href="" target="_blank">CODE</a>', $html);
+        $this->assertContains('<a href="" target="_blank">SEE ?</a>', $html);
     }
 
     public function testFailureisKeyExist() {
@@ -59,4 +74,3 @@ class Cms extends TestCase {
         $html = populateProject($projects);
     }
 }
-?>
